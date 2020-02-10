c39d42a934bedf0a1a16c3c1eabaf1a694b5f408
Dmitry Dluzhnevsky <dluzhnevsky@yahoo.com>
2020-02-10 22:51:56 +0300

    Initial Commit

---
+++ Loafy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Loafy
+//
+//  Created by Dei on 10.02.2020.
+//  Copyright © 2020 Solden Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Loafy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Loafy
+//
+//  Created by Dei on 10.02.2020.
+//  Copyright © 2020 Solden Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
